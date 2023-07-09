2b4951f08792cac78f84ea4547cf0c5bb7e3d62a
Suleiman Hajizadeh <suleyman.hacizade1@gmail.com>
2023-07-09 19:22:13 +0400

    8

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,16 +1,28 @@
 import { Component } from "@angular/core";
-import { Movies } from "../movie.datasource";
 import { Movie } from "../movie";
+import { MovieService } from "../movie.service";
 @Component({
     selector: 'movies',
     templateUrl: 'movies.component.html'
 })
 export class MoviesComponent {
     title = 'Movie List';
-    movies = Movies;
+    movies!: Movie[];
     selectedMovie!: Movie;
+
+    constructor(private movieService:MovieService) {}
+
+    ngOnInit(): void {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        this.getMovies();
+    }
 
     onSelect(movie: Movie): void {
         this.selectedMovie = movie;
+    }
+
+    getMovies(): void {
+        this.movies = this.movieService.getMovies()
     }
 }
