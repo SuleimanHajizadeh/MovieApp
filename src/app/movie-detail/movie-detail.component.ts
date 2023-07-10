5d759bfff15a0a19e5f432da53476d77546290ef
Suleiman Hajizadeh <suleyman.hacizade1@gmail.com>
2023-07-10 19:15:42 +0400

    13-14

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,13 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.css']
 })
-export class MovieDetailComponent {
-
+export class MovieDetailComponent implements OnInit {
+  // localhost:3000/detail/2
   @Input() movie!: Movie
 
+  constructor(
+    private movieService: MovieService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.getMovie();
+  }
+
+  getMovie(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.movieService.getMovie(id)
+      .subscribe(movie => this.movie = movie)
+  }
 }
