5ee1bcc0a58f7988adc658ea3dabc5e262473e90
Suleiman Hajizadeh <suleyman.hacizade1@gmail.com>
2023-07-14 01:50:11 +0400

    prepared

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -3,7 +3,7 @@ import { Movie } from './movie';
 import { Movies } from './movie.datasource';
 import { Observable, of } from 'rxjs';
 import { LoggingService } from './logging.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +15,7 @@ export class MovieService {
   constructor(
     private loggingService: LoggingService,
     private http: HttpClient
-    ) { }
+  ) { }
 
   getMovies(): Observable<Movie[]> {
     this.loggingService.add('MovieService: listing movies');
@@ -23,7 +23,22 @@ export class MovieService {
   }
 
   getMovie(id: number): Observable<Movie> {
-    this.loggingService.add('MovieService: get detail by id='+ id) 
-    return of(Movies.find(movie => movie.id === id));
+    this.loggingService.add('MovieService: get detail by id=' + id)
+    return this.http.get<Movie>(this.apiMoviesUrl + '/' + id)
+  }
+
+  update(movie: Movie): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'aplication/json' })
+    }
+    return this.http.put(this.apiMoviesUrl, movie, httpOptions);
+  }
+
+  add(movie: Movie): Observable<Movie> {
+    return this.http.post<Movie>(this.apiMoviesUrl, movie);
+  }
+
+  delete(movie: Movie): Observable<Movie> {
+    return this.http.delete<Movie>(this.apiMoviesUrl+'/'+movie.id);
   }
 }
